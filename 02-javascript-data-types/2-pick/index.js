ea7adfe1aacd53ea8f063eb3f16318c9ad8c235f
Olga Aiurova <kaorubest@yandex.ru>
2025-03-25 12:03:51 +0300

    changed object declaration: let -> const

--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -5,7 +5,7 @@
  * @returns {object} - returns the new object
  */
 export const pick = (obj, ...fields) => {
-    let newObj = {};
+    const newObj = {};
     Object.entries(obj).forEach(([key, value]) => {
         if (fields.indexOf(key) !== -1) {
             newObj[key] = value;
