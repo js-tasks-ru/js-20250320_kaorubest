e24651574aea666650a4c799005188f66fcd7317
Olga Aiurova <kaorubest@yandex.ru>
2025-03-30 11:24:47 +0300

    getter by properties path implementation

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,15 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+    const propNames = path.split('.');
+    const size = propNames.length;
+    return (obj) => {
+        for (let i=0; i<size; i++) {
+            const prop = propNames[i];
+            if (prop in obj && Object.hasOwn(obj, prop)) {
+                obj = obj[prop];
+            } else return undefined
+        }
+        return obj;
+    }
 }
