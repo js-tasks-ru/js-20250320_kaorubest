5468c06b5f1435326368f0cf0e67f4f69b394924
Olga Aiurova <kaorubest@yandex.ru>
2025-03-30 11:25:23 +0300

    invert object props  implementation

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,10 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
-
+    if (obj === undefined) return undefined;
+    const inverted = {};
+    for (const [key, value] of Object.entries(obj)) {
+        inverted[value] = key
+    }
+    return inverted;
 }
